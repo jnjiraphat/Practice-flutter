0c9981a38399e8b175906028929bcf1b063ea9d9
jnjenny <jnjiraphat@gmail.com>
2020-06-11 14:11:21 +0700

    add daisy card

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,53 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('My App')
       ),
-      body: Center(child: Text('Hi my App'),),
+      body: MyBody()
+    );
+  }
+}
+
+class MyBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Card(
+              margin: EdgeInsets.all(50),
+              elevation: 10,
+              child: Column(
+              children: <Widget>[
+                Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage('https://ci.lnwfile.com/j5fe4q.jpg')
+                    )
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Daisy',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),),
+                ),
+                Container(
+                  alignment: Alignment(-1, 0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'The daisy came by symbolize childbirth, motherhood, and new beginnings',
+                    ),
+                  ),
+                )
+              ]
+            ),
+        ),
+      ],
     );
   }
 }
