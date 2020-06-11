ba8faacd2ffb42669a61a7abc67b68873f87321b
jnjenny <jnjiraphat@gmail.com>
2020-06-11 14:54:52 +0700

    set props card

--- lib/main.dart
+++ lib/main.dart
@@ -33,45 +33,64 @@ class MyHomePage extends StatelessWidget {
 class MyBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: <Widget>[
-        Card(
-              margin: EdgeInsets.all(50),
-              elevation: 10,
-              child: Column(
-              children: <Widget>[
-                Container(
-                  height: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage('https://ci.lnwfile.com/j5fe4q.jpg')
-                    )
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Daisy',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                    ),),
-                ),
-                Container(
-                  alignment: Alignment(-1, 0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'The daisy came by symbolize childbirth, motherhood, and new beginnings',
-                    ),
-                  ),
-                )
-              ]
-            ),
-        ),
+        FlowerCard('Daisy','hey','https://ci.lnwfile.com/j5fe4q.jpg'),
+        FlowerCard('Rose','hey','https://transcode2.app.engoo.com/image/fetch/f_auto,c_lfill,w_800,h_600,dpr_3/https://assets.app.engoo.com/images/mSmbmA5J74lqS1VzMlODggUCwSQtLl69ggFzMDmLRme.jpeg'),
+        FlowerCard('Sunflower','hey','https://www.tcs-mart.com/web/wp-content/uploads/2018/02/sunflower-1627193_960_720-600x600.jpg'),
       ],
+    );
+  }
+}
+
+class FlowerCard extends StatelessWidget {
+
+  final String _text;
+  final String _img;
+  final String _content;
+  
+
+  const FlowerCard(this._text,this._content,this._img,{
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+          margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          elevation: 10,
+          child: Column(
+          children: <Widget>[
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage('$_img')
+                )
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '$_text',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),),
+            ),
+            Container(
+              alignment: Alignment(-1, 0),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '$_content',
+                ),
+              ),
+            )
+          ]
+        ),
     );
   }
 }
